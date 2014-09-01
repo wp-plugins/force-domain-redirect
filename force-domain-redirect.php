dcca39e60fc55e130c516c6273ec4f61249477f6
nimusdigital <nimusdigital@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-09-01 09:35:46 +0000

    Version 0.1 (initial release)
    git-svn-id: https://plugins.svn.wordpress.org/force-domain-redirect/trunk@976332 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ force-domain-redirect.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Plugin Name: Force Domain Redirect
+ * Plugin URI: http://nimbus.agency
+ * Description: Forces the use of the main WP domain
+ * Version: 0.1
+ * Author: Mark Williams
+ * Author URI: http://nimbus.agency
+ * License: GPL2
+ */
+	 /*
+	 Copyright 2014  Mark Williams  (email : wordpress.org@tracemail.co.uk)
+
+	 This program is free software; you can redistribute it and/or modify
+	 it under the terms of the GNU General Public License, version 2, as 
+	 published by the Free Software Foundation.
+
+	 This program is distributed in the hope that it will be useful,
+	 but WITHOUT ANY WARRANTY; without even the implied warranty of
+	 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	 GNU General Public License for more details.
+
+	 You should have received a copy of the GNU General Public License
+	 along with this program; if not, write to the Free Software
+	 Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+	 */
+	 
+
+	 function redirect_URL(){
+		$uri=$_SERVER['REQUEST_URI'];
+		$domain=$_SERVER['HTTP_HOST'];
+		$wpDomain=get_site_url();
+		if($wpDomain=="http://".$domain){
+			//echo "You are at the correct domain";
+		} else {
+			Header( "HTTP/1.1 301 Moved Permanently" );
+			Header( "Location: ".$wpDomain.$uri );
+			echo $domain." isn't the right domain: ".$wpDomain;
+			die();
+			
+		}
+	 }
+add_action('init', 'redirect_URL');
+?>
---
+++ readme.txt
@@ -0,0 +1,23 @@
+=== Force Domain Redirect ===
+Contributors: nimbusdigital
+Tags: domain,redirect,301,url,uri,rewrite
+Requires at least: 3.0
+Tested up to: 3.9.2
+License: GP2
+
+Forces your Wordpress site to load on the domain registered in the WP admin.
+
+== Description ==
+Redirects http://somedomain.tld to http://someotherdomain.tld
+
+Assuming you have 2 or more domains in use for a single Wordpress site. This plugin forces the use of only one no matter which is entered in the address bar.
+
+If, for some reason, your host won’t automatically redirect from one domain to another, then this plugin will do it. It checks the URL used to access the site and does a 301 redirect if incorrect.
+
+In theory htaccess should do this, but in my case it didn’t. Perhaps because I am using domain aliases (or CNAMEs) and I wanted to insure that the main one was always in use, even when accessed using old links.
+
+== Installation ==
+Copy the plugin file to your Wordpress plugins directory. That\'s it. No configuration necessary... just make sure the domain you want to force is listed under SETTINGS > GENERAL > WordPress Address (URL)
+
+== Changelog ==
+0.1 Initial version. Seems to do what\'s needed.
